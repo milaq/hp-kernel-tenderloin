eccc7fc774e787103f9908cfd4f3fd2cdf4677ef
koalala <vbelo@hotmail.fr>
2014-02-26 21:37:54 +0000

    Setting camera sensor GPIOs

--- arch/arm/mach-msm/gpiomux-8x60.c
+++ arch/arm/mach-msm/gpiomux-8x60.c
@@ -2440,6 +2440,7 @@ tenderloin_gpiomux_cfgs[] __initdata = {
 	{msm8x60_snd_configs, ARRAY_SIZE(msm8x60_snd_configs)},
 	{msm8x60_aux_pcm_configs, ARRAY_SIZE(msm8x60_aux_pcm_configs)},
 	{msm8x60_mdp_vsync_configs, ARRAY_SIZE(msm8x60_mdp_vsync_configs)},
+	{msm8x60_cam_configs, ARRAY_SIZE(msm8x60_cam_configs)},
 #if 0
 #if defined(CONFIG_USB_PEHCI_HCD) || defined(CONFIG_USB_PEHCI_HCD_MODULE)
 	{tenderloin_ebi2_configs, ARRAY_SIZE(tenderloin_ebi2_configs)},
